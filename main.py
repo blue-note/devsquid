97804bfe5cb57e907f406c1bac9c2dc5076af3de
Janita Chalam <janitachalam@Janitas-MacBook-Pro.local>
2015-08-09 15:11:53 -0700

    deploy heroku

---
+++ main.py
@@ -0,0 +1,18 @@
+import tweepy, sys, time
+import requests
+ 
+def tweet():
+    consumer_key = 'AThMTxBQ3TUxAWQ5iN1gZ0JKj'
+    consumer_secret = 'pQdvykOviv7U197tGVkAyBneIOQPBk7l1cVcLv4T7sB3n5SCG8'
+    access_key = '3386176895-g0yaSvp1fZKW9mJs67iyzSMYXmoa1jCFhAgonau'
+    access_secret = 'IWGXdN7D9hk1agu5eepaFOF7PZ6MlMyJHlGK5tMYESfFs'
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    query = "tech"
+    response = api.search(q=query,rpp=1,count=1)
+    print response[0].text
+    api.update_status(status=response[0].text)
+
+if __name__ == "__main__":
+    tweet()
