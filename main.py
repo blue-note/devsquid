425b79520e5c217dae307ad7f64f75e5918e5df4
Janita Chalam <janitachalam@Janitas-MacBook-Pro.local>
2015-08-16 04:19:03 -0700

    rm Procfile

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import tweepy, sys, time
-import requests
+import tweepy, sys, time, requests, os
  
 def tweet():
-    consumer_key = 'AThMTxBQ3TUxAWQ5iN1gZ0JKj'
-    consumer_secret = 'pQdvykOviv7U197tGVkAyBneIOQPBk7l1cVcLv4T7sB3n5SCG8'
-    access_key = '3386176895-g0yaSvp1fZKW9mJs67iyzSMYXmoa1jCFhAgonau'
-    access_secret = 'IWGXdN7D9hk1agu5eepaFOF7PZ6MlMyJHlGK5tMYESfFs'
+    keys = open('config.txt', 'r')
+    keys = keys.read().splitlines()
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+    access_key = os.environ['ACCESS_KEY']
+    access_secret = os.environ['ACCESS_SECRET']
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
-    query = "tech"
+    query = "node"
     response = api.search(q=query,rpp=1,count=1)
     api.update_status(status=response[0].text)
     
