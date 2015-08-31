bb94c232189b298d7ad1df46cfcf77c97dd58d80
Janita Chalam <janitachalam@gmail.com>
2015-08-30 23:08:00 -0700

    ready for deploy

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tweepy, sys, time, requests, os
+import urllib
+import json as m_json
  
 def tweet():
-    keys = open('config.txt', 'r')
-    keys = keys.read().splitlines()
+    google_query = "janita chalam"
+    google_query = urllib.urlencode({'q' : google_query})
+    response = urllib.urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + google_query).read()
+    json = m_json.loads(response)
+    results = json['responseData']['results']
+    for result in results:
+        title = result['title']
+        url = result['url'] 
+        print (url)
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     access_key = os.environ['ACCESS_KEY']
@@ -12,7 +21,7 @@ def tweet():
     api = tweepy.API(auth)
     query = "node"
     response = api.search(q=query,rpp=1,count=1)
-    api.update_status(status=response[0].text)
+    # api.update_status(status=response[0].text)
     
 if __name__ == "__main__":
     tweet()
